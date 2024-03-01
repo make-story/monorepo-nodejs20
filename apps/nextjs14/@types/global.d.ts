22ea2ed2d99a9a3af59993b431fc3cebd02448c6
sung-min <yu9221@gmail.com>
2024-03-01 18:20:36 +0900

    update

---
+++ apps/nextjs14/@types/global.d.ts
@@ -0,0 +1,19 @@
+export declare global {
+  export interface Window {
+    /* SNS 공유 */
+    Kakao: any;
+    kakao: any;
+
+    /*네이버 페이*/
+    naver: any;
+
+    /*ios 앱*/
+    webkit: any;
+
+    /* webview */
+    CustomEvent: any;
+
+    /* 기타 */
+    microfrontend: any;
+  }
+}
