dbbc7f0bbcf645837bd0b303c30ff7fd418ced08
sung-min <yu9221@gmail.com>
2024-03-03 18:34:32 +0900

    update

---
+++ packages/event-manager/src/observer/index.ts
@@ -0,0 +1,20 @@
+/**
+ * IntersectionObserver
+ * 뷰포트에서 요소 가시성 감지
+ * https://developer.mozilla.org/ko/docs/Web/API/IntersectionObserver
+ * 레이지로딩, 무한스크롤
+ *
+ * MutationObserver
+ * DOM 트리의 변경을 감지
+ * https://developer.mozilla.org/ko/docs/Web/API/MutationObserver
+ * DOM 추가/제거/수정 시 이벤트버스 등 콜백
+ *
+ * ResizeObserver
+ * https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserver
+ *
+ * PerformanceObserver
+ * https://developer.mozilla.org/en-US/docs/Web/API/PerformanceObserver
+ */
+
+const intersection = {};
+const mutation = {};
