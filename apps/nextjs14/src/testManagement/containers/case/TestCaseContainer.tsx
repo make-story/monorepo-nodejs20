16e80d0a0a2c9e181ebc70deb9951dc7737bfc0e
sung-min <yu9221@gmail.com>
2024-03-13 23:09:16 +0900

    update

--- apps/nextjs14/src/testManagement/containers/case/TestCaseContainer.tsx
+++ apps/nextjs14/src/testManagement/containers/case/TestCaseContainer.tsx
@@ -3,6 +3,13 @@ import { PropsWithChildren } from 'react';
 import TestCaseRun from '@/testManagement/components/case/TestCaseRun';
 
 const TestCaseContainer = (props: PropsWithChildren) => {
+  /**
+   * 비즈니스 로직 및 이벤트 관리
+   * 소켓 연결(nav 에서 선택된 값 전달)
+   * 소켓에서 받은 내용 각 컴포넌트로 전달
+   */
+  // 웹소켓 사용자훅
+  const useTestCaseWebSoket = () => {};
   return (
     <>
       <TestCaseRun />
