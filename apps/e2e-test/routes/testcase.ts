7b92b4820947b1568f3a1640d8977058654df669
[상품상세개발팀] 유성민 <sungmin.yu01@lotte.net>
2024-03-13 20:44:43 +0900

    update

--- apps/e2e-test/routes/testcase.ts
+++ apps/e2e-test/routes/testcase.ts
@@ -8,10 +8,25 @@ import { getDirectoryFile } from '#/utils/fileSystem';
 
 const router = express.Router();
 
+// category 반환
 router.get(
-  '/list',
+  '/category',
   (request: Request, response: Response, next: NextFunction) => {
-    const { file } = getDirectoryFile('headlessbrowser/testcase', {
+    const { directory } = getDirectoryFile('headlessbrowser/testcase');
+
+    //return next();
+    return response.json({
+      data: directory,
+    });
+  },
+);
+
+// category 의 테스트케이스 리스트 반환
+router.get(
+  '/:category/list',
+  (request: Request, response: Response, next: NextFunction) => {
+    const { category } = request.params;
+    const { file } = getDirectoryFile(`headlessbrowser/testcase/${category}`, {
       isFileExtension: false,
     });
 
